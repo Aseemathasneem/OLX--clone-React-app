be09cd44e3259e645c7217601c590c5e91c974f3
Aseema thasneem <aseemathasneem@gmail.com>
2024-02-04 00:53:33 +0530

    sign up completed

---
+++ src/Components/Signup/Signup.js
@@ -0,0 +1,107 @@
+import React, { useContext, useState } from 'react';
+
+import Logo from '../../olx-logo.png';
+import './Signup.css';
+import { FirebaseContext } from '../../store/Context';
+import { getAuth,createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { firestore } from '../../firebase/config';
+import { addDoc, collection } from 'firebase/firestore';
+
+export default function Signup() {
+  const [username,setUsername] = useState('')
+  const [email,setEmail] = useState('')
+  const [phone,setPhone]  = useState('')
+  const [password,setPassword] = useState('')
+  const {firebase}  = useContext(FirebaseContext)
+
+  const auth = getAuth();
+    const handleSubmit = (e)=>{
+      e.preventDefault()
+      createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      updateProfile(user, { displayName: username })
+        .then(() => {
+          // Successfully updated display name
+          
+          addDoc(collection(firestore, 'users'), {
+            id: user.uid,
+            username: username,
+            phone: phone,
+          })
+            .then(() => {
+              console.log('User details added to Firestore successfully');
+              
+            })
+            .catch((firestoreError) => {
+              console.error('Error adding user details to Firestore:', firestoreError);
+              // Handle error adding user details to Firestore
+            });
+        })
+        
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error('Signup error:', errorCode, errorMessage);
+      // Handle errors, e.g., display an error message to the user
+    });
+    }
+  return (
+    <div>
+      <div className="signupParentDiv">
+        <img width="200px" height="200px" src={Logo}></img>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="fname">Username</label>
+          <br />
+          <input
+            className="input"
+            type="text"
+            id="fname"
+            name="name"
+            value={username}
+            onChange={(e)=>setUsername(e.target.value)}
+          />
+          <br />
+          <label htmlFor="fname">Email</label>
+          <br />
+          <input
+            className="input"
+            type="email"
+            id="fname"
+            name="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+          <br />
+          <label htmlFor="lname">Phone</label>
+          <br />
+          <input
+            className="input"
+            type="number"
+            id="lname"
+            name="phone"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
+          />
+          <br />
+          <label htmlFor="lname">Password</label>
+          <br />
+          <input
+            className="input"
+            type="password"
+            id="lname"
+            name="password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+          />
+          <br />
+          <br />
+          <button>Signup</button>
+        </form>
+        <a>Login</a>
+      </div>
+    </div>
+  );
+}
---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+import 'firebase/auth';
+const firebaseConfig = {
+    apiKey: "AIzaSyB8YfZwdDloJuK1Fhp5WaJ8Ykuczm6nScs",
+    authDomain: "olx-clone-d2b8e.firebaseapp.com",
+    projectId: "olx-clone-d2b8e",
+    storageBucket: "olx-clone-d2b8e.appspot.com",
+    messagingSenderId: "383112754739",
+    appId: "1:383112754739:web:3f2114cd2f5af6c0e9d546"
+  };
+  const firebase = initializeApp(firebaseConfig);
+  const auth = getAuth(firebase);
+  const firestore = getFirestore(firebase);
+  export default firebase;
+  export {auth,firestore }
